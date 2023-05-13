1ae7ba3f3bba5f57ba96d870291265f5eecbde1c
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2023-05-13 00:55:34 -0300

    Update TRIPLE_PENDULUM_CHAOS_THEORY.py

--- TRIPLE_PENDULUM_CONVERSTION/TRIPLE_PENDULUM_CHAOS_THEORY.py
+++ TRIPLE_PENDULUM_CONVERSTION/TRIPLE_PENDULUM_CHAOS_THEORY.py
@@ -15,23 +15,13 @@ def deriv(t, y):
     
     return dθ1dt, dθ1pdt, dθ2dt, dθ2pdt, dθ3dt, dθ3pdt
 
-def kinetic_energy(vx1, vy1, vx2, vy2, vx3, vy3, m1, m2, m3): # Energia Cinética
-    return 0.5 * m1 * (vx1**2 + vy1**2) + 0.5 * m2 * (vx2**2 + vy2**2) + 0.5 * m3 * (vx3**2 + vy3**2)
-
-def gravitational_potential_energy(y1, y2, y3, m1, m2, m3, g): # Energia potêncial gravitacional
-    return m1 * g * y1 + m2 * g * y2 + m3 * g * y3
-
-def mechanical_energy(vx1, vy1, vx2, vy2, vx3, vy3, y1, y2, y3, m1, m2, m3, g): # Energia mecânica
-    return kinetic_energy(vx1, vy1, vx2, vy2, vx3, vy3, m1, m2, m3) + gravitational_potential_energy(y1, y2, y3, m1, m2, m3, g) + 0
-
-
 # Definindo as propriedades
-m1 = 1.0 # Massa do pêndulo 1 (kg)
-m2 = 1.0 # Massa do pêndulo 2 (kg)
-m3 = 1.0 # Massa do pêndulo 3 (kg)
-l1 = 1.0 # Comprimento da haste 1 (m)
-l2 = 1.0 # Comprimento da haste 2 (m)
-l3 = 1.0 # Comprimento da haste 3 (m)
+m1 = 1 # Massa do pêndulo 1 (kg)
+m2 = 1 # Massa do pêndulo 2 (kg)
+m3 = 1 # Massa do pêndulo 3 (kg)
+l1 = 1 # Comprimento da haste 1 (m)
+l2 = 10 # Comprimento da haste 2 (m)
+l3 = 1 # Comprimento da haste 3 (m)
 g = 10 # Aceleração da gravidade (m/s^2)
 
 # Definindo as condições iniciais do sistema
@@ -44,35 +34,10 @@ g = 10 # Aceleração da gravidade (m/s^2)
 
 y0 = [θ1_0, θ1p_0, θ2_0, θ2p_0, θ3_0, θ3p_0] # Junta os parâmetros iniciais
 
-t_span = (0.0, 10.0) # Define o tempo de intervalo
-t = np.linspace(0, 40, 1000) # Cria um array de mil pontos entre 0 até 40 segundos
+t_span = (0.0, 40.0) # Define o tempo de intervalo
+t = np.linspace(0, 50,3) # Cria um array de mil pontos entre 0 até 40 segundos
 sol = solve_ivp(deriv, t_span, y0, t_eval=np.linspace(t_span[0], t_span[1], 500)) # Faz o cálculo diferencial a partir da função 'deriv'
 
-# Criação de listas para armazenar os valores de cada energia 
-kinetic_energies = [] 
-potential_energies = [] 
-mechanical_energies = [] 
-
-for i in range(len(sol.t)):
-    y = sol.y[:, i]
-    θ1, θ1p, θ2, θ2p, θ3, θ3p = y
-    
-    # Calcular velocidades
-    vx1 = l1*θ1p*np.cos(θ1)
-    vy1 = l1*θ1p*np.sin(θ1)
-    vx2 = vx1 + l2*θ2p*np.cos(θ2)
-    vy2 = vy1 + l2*θ2p*np.sin(θ2)
-    vx3 = vx2 + l3*θ3p*np.cos(θ3)
-    vy3 = vy2 + l3*θ3p*np.sin(θ3)
-    
-    # Calcular energias
-    kinetic = kinetic_energy(vx1, vy1, vx2, vy2, vx3, vy3, m1, m2, m3)
-    potential = gravitational_potential_energy(-l1*np.cos(θ1), -l2*np.cos(θ2), -l3*np.cos(θ3), m1, m2, m3, g)
-    mechanical = mechanical_energy(vx1, vy1, vx2, vy2, vx3, vy3, -l1*np.cos(θ1), -l2*np.cos(θ2), -l3*np.cos(θ3), m1, m2, m3, g)
-    
-    kinetic_energies.append(kinetic)
-    potential_energies.append(potential)
-    mechanical_energies.append([mechanical])
 
 #Gráfico 1
 plt.figure(figsize=(20, 12))
@@ -85,27 +50,3 @@ plt.legend()
 plt.grid()
 plt.show()
 
-#Gráfico 2
-fig, ax = plt.subplots()
-
-ax.plot(sol.y[0], sol.y[1], label='Pêndulo 1')
-ax.plot(sol.y[2], sol.y[3], label='Pêndulo 2')
-ax.plot(sol.y[4], sol.y[5], label='Pêndulo 3')
-
-ax.set_xlabel('Ângulo θ')
-ax.set_ylabel('Velocidade angular dθ/dt')
-ax.set_title('Trajetórias dos pêndulos')
-ax.legend()
-plt.grid()
-plt.show()
-
-#Gráfico 3
-plt.figure(figsize=(12, 8))
-plt.plot(sol.t, kinetic_energies, label='Energia cinética')
-plt.plot(sol.t, potential_energies, label='Energia potencial')
-#plt.plot(sol.t, mechanical_energies, label='Energia mecânica') #não conservativa pois está sendo influenciada pela gravidade
-plt.xlabel('Tempo (s)')
-plt.ylabel('Energia (J)')
-plt.legend()
-plt.grid()
-plt.show()
